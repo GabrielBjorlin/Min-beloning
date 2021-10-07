f81761c2fbbfd974be4bba2c2261f0323fdf329b
Camilla Heiding <camilla.heiding@tietoevry.com>
2021-10-07 19:13:20 +0200

    Färg och stjärnor på beloningar sida

--- pages/beloningar.js
+++ pages/beloningar.js
@@ -1,6 +1,7 @@
 import { Grid, Typography, Paper } from "@material-ui/core"
 import React from "react"
-import styles from "../styles/Home.module.css"
+import styles from "../styles/beloningar.module.css"
+import StarIcon from '@mui/icons-material/Star';
 
 const beloningar = () => {
   return (
@@ -9,17 +10,17 @@ const beloningar = () => {
       <Grid container style={{ marginTop: "30px" }}>
         <Grid item xs={12}>
           <Paper className={styles.paper}>
-            <Typography variant="h6">Biobiljett</Typography>
+            <Typography variant="h6" style={{display: 'inline'}}>Biobiljett 100</Typography><StarIcon style={{fontSize: "17px"}} />
           </Paper>
         </Grid>
         <Grid item xs={12}>
           <Paper className={styles.paper}>
-            <Typography variant="h6">Utflykt</Typography>
+            <Typography variant="h6" style={{display: 'inline'}}>Utflykt 50</Typography><StarIcon style={{fontSize: "17px"}} />
           </Paper>
         </Grid>
         <Grid item xs={12}>
           <Paper className={styles.paper}>
-            <Typography variant="h6">Exempel</Typography>
+            <Typography variant="h6" style={{display: 'inline'}}>Exempel 10</Typography><StarIcon style={{fontSize: "17px"}} />
           </Paper>
         </Grid>
       </Grid>
